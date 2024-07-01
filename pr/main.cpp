5071b5fe5cca8c2b4b2a4160668b472f5b24b92b
Paul <gerpaul588@gmail.com>
2024-07-01 13:44:58 +0300

    Initial Commit

---
+++ pr/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  pr
+//
+//  Created by paul blazhko on 01.07.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
